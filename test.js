02def7390a079fd30d7c05473468124ecfac614f
sunny <sunnyvales789@gmail.com>
2021-10-23 16:10:52 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-
-what is submitted data?
-<form action="/processing.html" method="GET"> <input type="hidden" id="username" value="my_value"> <button>click</button> </form>
-  
-  What is DoctTYPE
-What are the HTML5 tags
-What is semantic HTML
