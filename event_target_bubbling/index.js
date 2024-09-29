1ac5f2063984ef438ae32249516b5b4a980d6404
Gupta, Sunny <sunnyvales789@gmail.com>
2024-09-29 08:49:28 +0530

    update

--- event_target_bubbling/index.js
+++ event_target_bubbling/index.js
@@ -1,13 +1,15 @@
-  // window.onload = function() {
-    var resultElem = document.getElementById('result')
-    document.getElementById('1').addEventListener(
-      'click',
-      function(event) {
-        resultElem.innerHTML += ('<div>target: ' + event.target['id'] + '</div>')
-        resultElem.innerHTML += ('<div>currentTarget: ' + event.currentTarget['id'] + '</div>')
-      },
-      false
-    )
-    document.getElementById('2').dispatchEvent(
-            new Event('click', { bubbles:true }))
-  // }
+// window.onload = function() {
+var resultElem = document.getElementById("result");
+document.getElementById("1").addEventListener(
+  "click",
+  function (event) {
+    resultElem.innerHTML += "<div>target: " + event.target["id"] + "</div>";
+    resultElem.innerHTML +=
+      "<div>currentTarget: " + event.currentTarget["id"] + "</div>";
+  },
+  false
+);
+document
+  .getElementById("2")
+  .dispatchEvent(new Event("click", { bubbles: true }));
+// }
