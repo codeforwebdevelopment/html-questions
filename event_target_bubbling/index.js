5b822e86f26b2c65c7551266fc4cc21c2cb69f3b
Gupta, Sunny <sunnyvales789@gmail.com>
2024-11-14 09:29:57 +0530

    update

--- event_target_bubbling/index.js
+++ event_target_bubbling/index.js
@@ -13,3 +13,83 @@ document
   .getElementById("2")
   .dispatchEvent(new Event("click", { bubbles: true }));
 // }
+
+function parentClick(){
+  console.log("Parent clicked");
+  }
+  
+ function childClick(){
+  console.log("child clicked");
+  }
+  
+  // If we click on this element it will give this output-
+  
+  // child clicked
+  
+  // Parent clicked
+
+  // To Stop Event Bubbling means to prevent calling parent element we use stopPropagation method.
+
+// Example-
+
+
+function parentClick(){
+console.log("Parent clicked");
+}
+
+function childClick(event){
+event.stopPropagation()
+console.log("child clicked");
+}
+
+// So, now when you click on this button it will give an output
+
+// child clicked
+
+function getEventTarget(e) {
+  e = e || window.event;
+  return e.target || e.srcElement;
+  }
+  
+  function editCell(e) {
+  var target = this.getEventTarget(e);
+  console.log(target)
+  if(target.tagName.toLowerCase() === 'td') {
+  // DO SOMETHING WITH THE CELL
+  }
+  }
+
+
+  const divs = Array.from(document.querySelectorAll('div'));
+        
+  const button = document.querySelector('button');
+  
+  function logText(e) {
+  
+  console.log(this.classList.value);
+  
+  e.stopPropagation(); // stop bubbling!
+  
+  // console.log(this);
+  
+  }
+  
+  divs.forEach(div => div.addEventListener('click', logText, {
+  
+  capture: false,
+  
+  once: true
+  
+  }));
+  
+  button.addEventListener('click', () => {
+  
+  console.log('Click!!!');
+  
+  }, {
+  
+  once: true
+  
+  });
+  
+  
